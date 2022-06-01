1c39b41eb359cf8cb8b34c6654b67e7447d02b55
ivershuo <shuo@aerfa.net>
2022-06-01 19:23:44 +0800

    remove vite

--- server/view.js
+++ server/view.js
@@ -1,8 +1,7 @@
 import { resolve } from 'path';
 import { existsSync, createReadStream } from 'fs';
 
-const devServer = 'http://' + process.env.HOST + ':' + process.env.VITE_PORT;
-const wwwRoot = resolve(process.env.DIST_PATH, 'www');
+const wwwRoot = resolve(process.env.DIST_PATH);
 const indexFile = 'index.html';
 
 /** 读取文件输出到 http response */
@@ -24,7 +23,7 @@ export function sendStatic(ctx, file) {
 /** 渲染页面 */
 export async function render(ctx) {
   if (process.env.MODE === 'dev') {
-    ctx.redirect(devServer + ctx.url);
+    sendStatic(ctx, resolve(wwwRoot, '..', 'www', 'index.html'));
   } else {
     sendStatic(ctx);
   }
